4d9b7463567bdb6c50c20492e304fcd23349692c
junio <junioeusebio@yahoo.com.br>
2024-11-19 22:37:57 -0300

    feat: ajustes gerais

--- src/app/vendas/vendas.component.ts
+++ src/app/vendas/vendas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IWebData } from '../google-tag-manager/google-analytics.interface';
+import { Sites } from './site.emun';
 
 @Component({
   selector: 'app-vendas',
@@ -61,48 +62,35 @@ export class VendasComponent implements OnInit {
     switch (modelo) {
       case 't1':
         this.gtmPushPurcase('t1');
-        window.open(
-          'https://www.ton.com.br/checkout/cart/?referrer=9EC6E894-77E9-4CB7-ABC3-ECAD34207106&userTag=tonpro_tier&productId=TONPRO_TIER_D150&userAnticipation=1'
-        );
+        window.open("//" + Sites.T1, '_blank');
         break;
       case 't3p':
         this.gtmPushPurcase('t3p');
-        window.open(
-          'https://www.ton.com.br/checkout/cart/?referrer=9EC6E894-77E9-4CB7-ABC3-ECAD34207106&userTag=tonpro_tier&productId=TONPRO_TIER_SMART_POS&userAnticipation=1'
-        );
+        window.open("//" + Sites.T3P, '_blank');
         break;
       case 't3':
         this.gtmPushPurcase('t3');
-        window.open(
-          'https://www.ton.com.br/checkout/cart/?referrer=9EC6E894-77E9-4CB7-ABC3-ECAD34207106&userTag=tonpro_tier&productId=TONPRO_TIER_S920&userAnticipation=1'
-        );
+        window.open("//" + Sites.T3P, '_blank');
         break;
       case 't2':
         this.gtmPushPurcase('t2');
-        window.open(
-          'https://www.ton.com.br/checkout/cart/?referrer=9EC6E894-77E9-4CB7-ABC3-ECAD34207106&userTag=tonpro_tier&productId=TONPRO_TIER_D195&userAnticipation=1'
-        );
+        window.open("//" + Sites.T2, '_blank');
         break;
       case 'insta':
-       // this.gtmPushPurcase('insta');
-        window.open('https://www.instagram.com/_stonparceiro');
+        this.gtmPushPurcase('insta');
+        window.open("//" + Sites.INSTAGRAM, '_blank');
         break;
       case 'whatsapp':
-       // this.gtmPushPurcase('whatsapp');
-        window.open('https://wa.me/5511930960173');
+        this.gtmPushPurcase('whatsapp');
+        window.open("//" + Sites.WHATSAPP, '_blank');
         break;
       case 'default':
         this.gtmPushPurcase('default');
-        window.open(
-          'https://www.ton.com.br/checkout/cart/?referrer=9EC6E894-77E9-4CB7-ABC3-ECAD34207106&userTag=tonpro_tier&productId=TONPRO_TIER_D195&userAnticipation=1'
-        );
+        window.open("//" + Sites.DEFAULT, '_blank');
         break;
       default:
         this.gtmPushPurcase('default');
-        window.open(
-          'https://www.ton.com.br/catalogo/?referrer=9EC6E894-77E9-4CB7-ABC3-ECAD34207106&utm_medium=invite_share&utm_source=revendedor',
-          '_blank'
-        );
+        window.open("//" + Sites.DEFAULT, '_blank');
         break;
     }
   }
